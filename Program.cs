6561dc530a1bb99fa1f61af762dfb14a8d8f7283
natalia shishkevich <shel_12@mail.ru>
2022-08-25 23:08:56 +0300

    Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. (Строки и циклы в решении использовать нельзя!)

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. (Строки и циклы в решении использовать нельзя!)
+//645 -> 6
+//78 -> третьей цифры нет
+//32679 -> 6
+Console.Write("Введите число: ");
+int NumberB = int.Parse(Console.ReadLine());
+int result1 = NumberB / 10;
+int result2 = result1 / 10;
+int result = result2 % 10;
+if (NumberB < 100)
+Console.WriteLine("Третьей цифры нет");   
+else
+Console.WriteLine($"Третья цифра {result}");
